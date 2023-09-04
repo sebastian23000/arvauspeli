74e3ca8271ccf4258af1d85531d1abc1e5528366
sebastian23000 <142879501+sebastian23000@users.noreply.github.com>
2023-09-04 18:29:51 +0300

    Basic functionality added.
    Created  a program, that asks user for a name once.

--- src/arvauspeli/ArvausPeli.java
+++ src/arvauspeli/ArvausPeli.java
@@ -1,7 +1,23 @@
 package arvauspeli;
 
+import java.util.Scanner;
+
 public class ArvausPeli {
 	public static void main(String[] args) {
+		String name = "Vilhelmiina";
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Arvaa nimi:");
+		String input = sc.nextLine();
+	
+		if (input.equals(name)) {
+			System.out.println("Onneksi olkoon! Arvasit nimen oikein.");
+		}
+		
+		else {
+			System.out.println("Väärin.");
+		}
+		
+		System.out.println("Oikea nimi oli " + name + ".");
 	}
 }
