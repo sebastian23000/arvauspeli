2f49cc2718771a4c86a319e99151c54ba257a045
sebastian23000 <142879501+sebastian23000@users.noreply.github.com>
2023-09-04 19:31:32 +0300

    Some improvements added
    3 major changes:
    - Now name is chosen randomly from array "names" instead of using the same name "Vilhelmiina" all the time.
    - If user guesses wrong 3 times they will get a hint. If user guesses wrong 3*nameLength times they will lose the game.
    - Input isn't anymore casesensitive. It doesn't anymore matter whether user enters "vilhelmiina" or "ViLhElMiInA".

--- src/arvauspeli/ArvausPeli.java
+++ src/arvauspeli/ArvausPeli.java
@@ -1,22 +1,43 @@
 package arvauspeli;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class ArvausPeli {
 	public static void main(String[] args) {
-		String name = "Vilhelmiina";
+		String[] names = {
+			"Aila", "Aili", "Alvar", "Ansa", "Arho", "Arja", "Auno",
+			"Eevert",
+			"Hellevi", "Hilla", "Hillevi",
+			"Iida", "IIta", "Isla", "Isto",
+			"Justus",
+			"Kaleva", "Kalevi", "Kuisma", "Kullervo",
+			"Lenni",
+			"Mainio", "Mauri", "Mervi", "Mielikki", "Miisa", "Mika", "Mikael", "Mikko", "Milo", "Minja", "Miro",
+			"Orvo",
+			"Pirkka",
+			"Reija", "Roni",
+			"Sandra", "Santeri", "Santtu", "Sirja", "Siru", "Sini", "Sinikka", "Sirpa", "Soila", "Soile", "Soili", "Sorja",
+			"Taimi", "Tytti", "Tyyne", "Tyyni",
+			"Valma", "Varpu", "Vaula", "Vertti", "Vesa", "Vilja"
+		};
+		
+		Random rn = new Random();
+		String name = names[rn.nextInt(names.length)];
 		String quit = "loppu";
 
 		Scanner sc = new Scanner(System.in);		
 		String input;
 		int countGuesses = 0;
+		int nameLength = name.length();
 	
 		System.out.println("Syötä \"" + quit + "\", kun haluat lopettaa.");
 		while (true) {
+			System.out.println("Sinulla on " + (nameLength*3 - countGuesses) + " yritystä.");
 			System.out.print("Arvaa nimi: ");
 			input = sc.nextLine();
 			
-			if (input.equals(quit)) {
+			if (input.equalsIgnoreCase(quit)) {
 				System.out.println("Ok. Lopetetaan...");
 				System.out.println("Ehdit tähän mennessä arvaamaan väärin " + countGuesses + " kertaa.");
 				break;
@@ -24,7 +45,7 @@ public class ArvausPeli {
 			
 			countGuesses++;
 			
-			if (input.equals(name)) {
+			if (input.equalsIgnoreCase(name)) {
 				System.out.println("Oikein!");
 				System.out.println("Onneksi olkoon! Vastasit oikein vastauskerralla " + countGuesses + ".");
 				break;
@@ -32,6 +53,22 @@ public class ArvausPeli {
 			
 			else {
 				System.out.println("Väärin! Ei ollut " + input + ".");
+				
+				if (countGuesses >= nameLength*3) {
+					System.out.println("Hävisit pelin, koska sinulla ei ole enään arvauskertoja.");
+					System.out.println("Ehdit tähän mennessä arvaamaan väärin " + countGuesses + " kertaa.");
+					break;
+				}
+				
+				//User get's a hint after 2nd wrong guess.
+				if (countGuesses == 2) {
+					System.out.println("Vinkki: Nimessä on " + nameLength + " kirjainta.");
+				}
+				
+				//User get's a hint every 3th wrong guess.
+				if (countGuesses%3 == 0) {
+					System.out.println("Vinkki: Nimi alkaa kirjaimilla \"" + name.substring(0, countGuesses/3) + "\".");
+				}
 			}
 		} 
 		
