595f1c9735e7e06ae4a08b35f2fb9f203e166c9b
sebastian23000 <142879501+sebastian23000@users.noreply.github.com>
2023-09-04 18:49:46 +0300

    Added loop functionality
    The user is now allowed to guess the name as many times as they want to. When the user enters "loppu" the infinite loop will break and the game will end. Also amount of guesses is counted.

--- src/arvauspeli/ArvausPeli.java
+++ src/arvauspeli/ArvausPeli.java
@@ -5,19 +5,38 @@ import java.util.Scanner;
 public class ArvausPeli {
 	public static void main(String[] args) {
 		String name = "Vilhelmiina";
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Arvaa nimi:");
-		String input = sc.nextLine();
+		String quit = "loppu";
+
+		Scanner sc = new Scanner(System.in);		
+		String input;
+		int countGuesses = 0;
 	
-		if (input.equals(name)) {
-			System.out.println("Onneksi olkoon! Arvasit nimen oikein.");
-		}
-		
-		else {
-			System.out.println("Väärin.");
-		}
+		System.out.println("Syötä \"" + quit + "\", kun haluat lopettaa.");
+		while (true) {
+			System.out.print("Arvaa nimi: ");
+			input = sc.nextLine();
+			
+			if (input.equals(quit)) {
+				System.out.println("Ok. Lopetetaan...");
+				System.out.println("Ehdit tähän mennessä arvaamaan väärin " + countGuesses + " kertaa.");
+				break;
+			}
+			
+			countGuesses++;
+			
+			if (input.equals(name)) {
+				System.out.println("Oikein!");
+				System.out.println("Onneksi olkoon! Vastasit oikein vastauskerralla " + countGuesses + ".");
+				break;
+			}
+			
+			else {
+				System.out.println("Väärin! Ei ollut " + input + ".");
+			}
+		} 
 		
 		System.out.println("Oikea nimi oli " + name + ".");
+		
+		sc.close();
 	}
 }
